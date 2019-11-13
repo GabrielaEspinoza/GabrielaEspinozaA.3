b449ef611f4947d2f150987f1ed0de0cd01cc288
GabrielaEspinoza <56659435+GabrielaEspinoza@users.noreply.github.com>
2019-11-12 20:39:01 -0500

    Add files via upload

---
+++ Cliente.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Empresa.Clases
+{
+
+
+    public class Cliente
+    {
+        public Cliente(string nombres, string apellidos, string numeroDeCedula, string direccion)
+        {
+            Nombres = nombres;
+            Apellidos = apellidos;
+            NumeroDeCedula = numeroDeCedula;
+            Direccion = direccion;
+        }
+
+        private string nombres;
+
+        public string Nombres
+        {
+            get { return nombres; }
+            set { nombres = value; }
+        }
+
+        private string apellidos;
+
+        public string Apellidos
+        {
+            get { return apellidos; }
+            set { apellidos = value; }
+        }
+
+        private string numeroDeCedula;
+
+        public string NumeroDeCedula
+        {
+            get { return numeroDeCedula; }
+            set { numeroDeCedula = value; }
+        }
+
+        private string direccion;
+
+        public string Direccion
+        {
+            get { return direccion; }
+            set { direccion = value; }
+        }
+
+
+    }
+}
